a6afcdfe15d7a4af82843a1b708b3a00d23764ab
Soga-Joshua <sogajoshua@gmail.com>
2022-04-30 18:53:27 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// for
+// var rice= document.getElementsByClassName("colorbot")[0];
+
+function MoveDiv(){
+    x=document.getElementById("color1");
+    x.style.display ="block";   
+}
+function RemoveDiv(){
+    x=document.getElementById("color1");
+    x.style.display="none";
+}
